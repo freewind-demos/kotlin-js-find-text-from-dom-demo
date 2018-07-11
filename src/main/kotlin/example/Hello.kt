4fe0cb0903e5e973aba3b9e11b5d84d94622c9c2
freewind <nowindlee@gmail.com>
2018-07-11 23:05:24 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,8 +1,35 @@
 package example
 
+import org.w3c.dom.*
+import kotlin.browser.document
+
 fun main(args: Array<String>) {
-    console.log(hello("Kotlin JS"))
+    val main = document.getElementById("main")!!
+    val allTexts = getAllNodesWithoutLinks(main).filterIsInstance<Text>().filterNot { it.wholeText.isBlank() }
+    println("allTexts(ignore links): ${allTexts.size}")
+    allTexts.forEachIndexed { index, node ->
+        console.log("$index:")
+        console.log(node)
+    }
 }
 
-fun hello(name:String) = "Hello, $name!"
+fun getAllNodesWithoutLinks(element: Element): List<Node> {
+    val all = mutableListOf<Node>(element)
+    var index = 0
+    while (index < all.size) {
+        val item = all[index]
+        val children = item.childNodes
+        for (i in 0 until children.length) {
+            children[i]?.run {
+                when (this) {
+                    is HTMLAnchorElement -> Unit  // ignore links
+                    else -> all.add(this)
+                }
+            }
+        }
+        index += 1
+    }
+    return all.toList()
+}
+
 
